95b319f841693aeb0a37cd9bf4df644c40070a77
javastrongX <likeprog1@gmail.com>
2025-09-12 09:59:46 +0500

    Update vite.config.js
    new full

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/', // 👉 bu SEO-friendly URL'lar uchun kerak
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   server: {
     host: '0.0.0.0',
     port: 5173,
